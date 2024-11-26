69258f214b9adcde452aab0085502e078489d361
engr-idemudia <idemudiamondayosaghae@gmail.com>
2024-11-26 20:10:33 +0200

    modify hero and agic button

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -30,7 +30,7 @@ const Hero = () => {
 
           <TextGenerateEffect
             className="text-center text-[40px] md:text-5xl lg:text5xl lg:text-6xl"
-            words="Tramsforming Concepts into Seamless Experiences"
+            words="Tramsforming Concepts into Seamless User Experiences"
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl ">
